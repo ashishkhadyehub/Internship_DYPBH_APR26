6e1c396587d26688071653f4838f384850c68c21
ashishkhadye33 <ashishkhadye33@gmail.com>
2026-05-13 12:27:47 +0530

    asp.net core session 3

---
+++ dotnetCore.Training/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using dotnetCore.Training.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnetCore.Training.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Students> Students { get; set; }
+
+
+    }
+}
